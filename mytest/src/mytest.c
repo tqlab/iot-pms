2857fd5cbbc90454aa299419ed18e1f11002397f
xmxsuperstar <xmxsuperstar@gmail.com>
2018-01-28 15:52:28 +0800

    test

--- mytest/src/mytest.c
+++ mytest/src/mytest.c
@@ -2,12 +2,26 @@
 // Created by lijun on 28/1/2018.
 //
 #include <inttypes.h>
+#include <time.h>
+#include <sys/time.h>
 #include "pmsutil.h"
 
 void main(){
 
-    printf("%"PRIu64"\n", pms_current_time_millis());
-    printf("%"PRId64"\n", pms_current_time_millis());
 
-    printf("%lld\n", pms_current_time_millis());
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+
+    uint64_t sec = tv.tv_sec;
+    uint64_t usec = tv.tv_usec;
+
+    printf("%"PRIu64"\n", sec);
+    printf("%"PRIu64"\n", usec);
+
+    uint64_t timestamp = sec * 1000 + usec / 1000;
+
+    printf("%"PRIu64"\n", timestamp);
+    printf("%"PRId64"\n", timestamp);
+
+    printf("%lld\n", timestamp);
 }
