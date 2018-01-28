b9f950a924289f7bd8c020ff100bcc406fe6551d
xmxsuperstar <xmxsuperstar@gmail.com>
2018-01-28 15:41:13 +0800

    test

--- mytest/src/mytest.c
+++ mytest/src/mytest.c
@@ -7,4 +7,7 @@
 void main(){
 
     printf("%" PRIu64 "\n", pms_current_time_millis());
+    printf("%" PRId64 "\n", pms_current_time_millis());
+
+    printf("%lld\n", pms_current_time_millis());
 }
