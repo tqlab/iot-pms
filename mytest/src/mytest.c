c52507fd788621b853f61fb4cf5bf5a434444c63
xmxsuperstar <xmxsuperstar@gmail.com>
2018-01-28 15:30:35 +0800

    uint64_t

--- mytest/src/mytest.c
+++ mytest/src/mytest.c
@@ -1,9 +1,10 @@
 //
 // Created by lijun on 28/1/2018.
 //
+#include <inttypes.h>
 #include "pmsutil.h"
 
 void main(){
 
-    printf("%ld", pms_current_time_millis());
+    printf("%" PRIu64 "\n", pms_current_time_millis());
 }
